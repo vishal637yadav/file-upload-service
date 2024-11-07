72b115e609f9ad904a21855b9b0dd89cac2cde67
vishalyadav <vishal637yadav@gmail.com>
2024-11-07 23:34:40 +0530

    Initial Commit

---
+++ src/main/java/com/code/file/upload/FileUploadServiceApplication.java
@@ -0,0 +1,13 @@
+package com.code.file.upload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileUploadServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileUploadServiceApplication.class, args);
+	}
+
+}
